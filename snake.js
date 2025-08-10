1b513bdb3e8d5431f0997632efdfde5e602e13ac
AlexLeLay <a.lelay663@laposte.net>
2025-08-10 22:07:59 +0200

    Snake body following the head, single tile only

--- snake.js
+++ snake.js
@@ -4,20 +4,27 @@
 
 // Arranger la position du corps du serpent après inputs
 
-// Bonus: ajouter un menu de sélection de
+// Bonus: ajouter un menu de sélection pour
 // la vitesse et de la couleur du serpent
-// comptage de points
-// 
+// Comptage de points
+// Tableau meilleurs scores avec BDD
 
-let speed = 3;
-let xDirection; // -1 vers la gauche, 1 vers la droite, 0 pour aucun
-let yDirection; // -1 vers le haut, 1 vers le bas, 0 pour aucun
+// stocker les inputs dans un tableau au cas où
+// ça se fait plus rapidement que le délacement du serpent
+// ou limiter à 1 input par frame
+
+
+let color = "green"; // On prendra l'input pour la couleur
+let speed = 1;       // et la vitesse
+let xDirection;      // -1 vers la gauche, 1 vers la droite, 0 pour aucun
+let yDirection;      // -1 vers le haut, 1 vers le bas, 0 pour aucun
 
 const cellSize = 20;
 let x = cellSize * 5;
 let y = cellSize * 5;
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
+let tile;
 const width = canvas.width = 400;
 const height = canvas.height = 400;
 
@@ -25,62 +32,122 @@ xDirection = 1;
 yDirection = 0;
 
 let snake = [
-    {x: x, y: y},
-    {x: x - cellSize, y: y},
-    {x: x - cellSize * 2, y: y}
+    {x: x, y: y, xDir: xDirection, yDir: yDirection},
+    {x: x - cellSize, y: y, xDir: xDirection, yDir: yDirection},
+    {x: x - cellSize * 2, y: y, xDir: xDirection, yDir: yDirection}
 ];
+
+// console.log(snake);
+
+// Ajouter un statut de case remplie pour éviter d'ajouter un fruit
+// sur le serpent
+// Fruit placé aléatoirement
+
+// Quand on mange un fruit push un objet dans snake[]
+// avec array.push()
+
+
+const head = snake[0];
+const tail = snake[snake.length - 1];
+// console.log(tail);
+
+// console.log(head);
+
 
 function draw() {
     if (canvas.getContext) {
         ctx.clearRect(0, 0, width, height);
 
-        snake.forEach((part) => {
-            part.x += cellSize * xDirection;
-            part.y += cellSize * yDirection;
+        if (tile) {
+            ctx.fillStyle = "red";
+            ctx.fillRect(tile.x, tile.y, cellSize, cellSize);
+            // console.log(tile);
+            for (let i = 1; i < snake.length; i++) {
+                if (snake[i].x == tile.x && snake[i].y == tile.y) {
+                    snake[i].xDir = tile.xDir;
+                    snake[i].yDir = tile.yDir;
+                }
+
+
+            }
+            
+        }
+
+        head.x += cellSize * head.xDir;
+        head.y += cellSize * head.yDir;
+
+        for (let i = 1; i < snake.length; i++) {
+            
+            snake[i].x += cellSize * snake[i].xDir;
+            snake[i].y += cellSize * snake[i].yDir;
+        }
+
+
+        for (let i = 0; i < snake.length; i++) {
+            
+
+
+
+            // console.log(snake[i].x);
+            
+
+
+
+
+
+
+
 
             // loop horizontal
-            if (part.x >= width) {
-                part.x = 0;
+            if (snake[i].x >= width) {
+                snake[i].x = 0;
             }
 
-            if (part.x < 0) {
-                part.x = width - cellSize;
+            if (snake[i].x < 0) {
+                snake[i].x = width - cellSize;
             }
 
             // loop vertical
-            if (part.y >= height) {
-                part.y = 0;
+            if (snake[i].y >= height) {
+                snake[i].y = 0;
             }
 
-            if (part.y < 0) {
-                part.y = height - cellSize;
+            if (snake[i].y < 0) {
+                snake[i].y = height - cellSize;
             }
 
-            ctx.fillStyle = "green";
-            ctx.fillRect(part.x, part.y, cellSize, cellSize);
+            ctx.fillStyle = color;
+            ctx.fillRect(snake[i].x, snake[i].y, cellSize, cellSize);
 
             
-        })
+        }
+
+        
         setTimeout(draw, 1000 / speed);
     }
 }
 
 draw();
 
+// Directions
+
+
 window.addEventListener("keydown", (key) => {
     if (yDirection == 0 ) {
         if (key.code === "ArrowDown") {
             xDirection = 0;
             yDirection = 1;
-            snake[0].x += cellSize * xDirection;
-            snake[0].y += cellSize * yDirection;
+            head.xDir = xDirection;
+            head.yDir = yDirection;
+            tile = {x: head.x, y: head.y, xDir: xDirection, yDir: yDirection};
         }
         
         if (key.code === "ArrowUp") {
             xDirection = 0;
             yDirection = -1;
-            snake[0].x += cellSize * xDirection;
-            snake[0].y += cellSize * yDirection;
+            head.xDir = xDirection;
+            head.yDir = yDirection;
+            tile = {x: head.x, y: head.y, xDir: xDirection, yDir: yDirection};
         }
     }
 
@@ -88,15 +155,17 @@ window.addEventListener("keydown", (key) => {
         if (key.code === "ArrowRight") {
             xDirection = 1;
             yDirection = 0;
-            snake[0].x += cellSize * xDirection;
-            snake[0].y += cellSize * yDirection;
+            head.xDir = xDirection;
+            head.yDir = yDirection;
+            tile = {x: head.x, y: head.y, xDir: xDirection, yDir: yDirection};
         }
         
         if (key.code === "ArrowLeft") {
             xDirection = -1;
             yDirection = 0;
-            snake[0].x += cellSize * xDirection;
-            snake[0].y += cellSize * yDirection;
+            head.xDir = xDirection;
+            head.yDir = yDirection;
+            tile = {x: head.x, y: head.y, xDir: xDirection, yDir: yDirection};            
         }
     }
 
